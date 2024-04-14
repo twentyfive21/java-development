97fe95789cd7bba9ff6ca1ae2c90b0bb7db2ff12
twentyfive21 <tina.winnn@gmail.com>
2024-04-14 19:28:23 -0400

    created cell phone constructor

---
+++ workbook-2/workbook2/src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,49 @@
+package com.pluralsight;
+
+public class CellPhone {
+    double serialNumber;
+    String model;
+    String carrier;
+    String phoneNumber;
+    String owner;
+
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    public double getSerialNumber() {
+        return this.serialNumber;
+    }
+    public void setSerialNumber(double serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    public String getModel() {
+        return this.model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getCarrier() {
+        return this.carrier;
+    }
+    public void setCarrier(String carrier){
+        this.carrier = carrier;
+    }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getOwner() {
+        return this.owner;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+}
---
+++ workbook-2/workbook2/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CellPhone firstUser = new CellPhone();
+    }
+}
