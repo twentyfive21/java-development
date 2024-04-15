a930e3dd4b2c1fe59a3cbce0c51d6d8bf7d16b54
twentyfive21 <tina.winnn@gmail.com>
2024-04-15 13:34:36 -0400

    looping exercises finished

--- workbook-2/workbook2/src/main/java/com/pluralsight/LoopingExercises.java
+++ workbook-2/workbook2/src/main/java/com/pluralsight/LoopingExercises.java
@@ -1,18 +1,42 @@
 package com.pluralsight;
 
 public class LoopingExercises {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException{
+            WhileLoop.main(args);
+            DoWhileLoop.main(args);
+            ForLoop.main(args);
     }
 
-    public class WhileLoop {
+    public static class WhileLoop {
         public static void main(String[] args) {
-
+            int i = 0;
+            while(i < 5) {
+                System.out.println((i+1) + ". I love java <3");
+                i++;
+            }
+            System.out.println("End of While Loop \n");
         }
     }
-    public class DoWhileLoop {
-        public static void main(String[] args) {
 
+    public static class DoWhileLoop {
+        public static void main(String[] args) {
+            int i = 0;
+            do {
+                System.out.println((i+1) +". I love java <3");
+                i++;
+            }  while (i < 5);
+            System.out.println("End of Do While Loop \n");
+        }
+    }
+
+    public static class ForLoop {
+        public static void main(String[] args) throws InterruptedException{
+            for(int i = 10; i >= 1; i--){
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+            System.out.println("!Launch!");
+            System.out.println("End of For Loop \n");
         }
     }
 }
