4e0f842f49c77d8aec0f8577ac0bca15d2b4193d
twentyfive21 <tina.winnn@gmail.com>
2024-04-21 17:17:17 -0400

    finished bedtime stories program

--- workbook-3/workbook-3/src/main/java/Mod01/BedtimeStories.java
+++ workbook-3/workbook-3/src/main/java/Mod01/BedtimeStories.java
@@ -4,33 +4,54 @@ import java.io.*;
 import java.util.Scanner;
 
 public class BedtimeStories {
+
     public static void main(String[] args) {
-        try{
-            // create a FileInputStream object pointing to a specific file
-            // give path to file input so it can find the txt file
-            // else it has no clue where the file is at
+
+        // initialize scanner to read user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please pick a number for which story you would like! \n"
+                +"(1) Goldilocks\n(2) Hansel & Gretel\n" +
+                "(3) Mary had a little lamb\n" +
+                "Selection: " );
+        String selection = scanner.nextLine();
+
+        // switch cases for all 3 stories and handles error if valid option not chosen
+        switch(selection){
+            case "1": selection = "goldilocks.txt";
+                break;
+            case "2": selection = "hansel_and_gretel.txt";
+                break;
+            case "3": selection = "mary_had_a_little_lamb.txt";
+                break;
+            default: System.out.println("Please choose a number from 1-3!");
+        }
+
+        //try to find the file to read and print out in the command line
+        try{// create a FileInputStream object pointing to a specific file
+            // give path to file input, so it can find the txt file
             FileInputStream fis = new FileInputStream("/Users/tina/" +
                     "pluralsight/java-development/workbook-3/workbook" +
-                    "-3/src/main/java/Mod01/goldilocks.txt");
+                    "-3/src/main/java/Mod01/" + selection);
             // create a Scanner to reference the file to be read
             /* When you create a Scanner, have it reference a file by
             wrapping the name of the file inside a FileInputStream
             object and passing that to the Scanner constructor */
-            Scanner scanner = new Scanner(fis);
+            Scanner scannerText = new Scanner(fis);
             String input;
             // read until there is no more data
             // hasNextLine() method returns true if there are more lines in the file
             // With nextLine(), you can read a line of text from the file
-            while(scanner.hasNextLine()) {
-                input = scanner.nextLine();
-                System.out.println(input);
+            int lineCounter = 1;
+            while(scannerText.hasNextLine()) {
+                input = scannerText.nextLine();
+                System.out.println(lineCounter + ". " +input);
+                lineCounter++;
             }
             // close the scanner & release the resources
-            scanner.close();
+            scannerText.close();
         } catch (IOException e) {
             // display stack trace if there was an error
             e.printStackTrace();
         }
         }
-
 }
