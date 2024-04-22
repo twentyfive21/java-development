8d69fe0ca29429fafcac432f119093e8bfb0c263
twentyfive21 <tina.winnn@gmail.com>
2024-04-22 14:54:49 -0400

    finished search engine logger

---
+++ workbook-3/workbook-3/src/main/java/Mod03/SearchEngineLogger.java
@@ -0,0 +1,79 @@
+package Mod03;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class SearchEngineLogger {
+
+
+    public static void main(String[] args) {
+        try{
+            /* Create a new BufferedWriter, wrapping around a FileWriter.
+
+             This code is initializing a BufferedWriter called writer which
+             will be used to write character data to a file.
+
+             Create a new FileWriter object to write to a file
+             Insert the path to the file you want to write to within the parentheses */
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/tina/pluralsight/" +
+                    "java-development/workbook-3/workbook-3/src/main/java" +
+                    "/Mod03/logs.txt"));
+            // ~~~~~~~~~~~~ Write to file on launch ~~~~~~~~~~~~
+            // Format date
+            // get thr current date and time
+            LocalDateTime actionTime =  LocalDateTime.now();
+            // chose format wanted
+            DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss ");
+            // take date made and pass to formatter
+            String formatedString = actionTime.format(fmt);
+            // write to the file
+            writer.write(formatedString + "First launching the application\n");
+            boolean isTrue = true;
+            while(isTrue){
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Please pick the following options: ");
+                System.out.println("a. Launching the application \n" +
+                        "b. Searching for a term\n" +
+                        "c. Exiting the application\n");
+                // saving users selection
+                System.out.print("Selection: ");
+                String selection = scanner.nextLine().toLowerCase().trim();
+
+                if (selection.equals("a")){
+                    System.out.println("You have launched the application :)");
+                    writer.write(formatedString + "User chose to launch the application\n");
+                } else if (selection.equals("b")){
+                    System.out.println("You have chosen to search for a term! :)");
+                    System.out.print("Enter a search term or (X to exit): ");
+                    String choice = scanner.nextLine().toLowerCase().trim();
+
+                    if(choice.equalsIgnoreCase("x")){
+                        System.out.println("You have chosen to exit. Please come again! :)");
+                        writer.write(formatedString + "Exit");
+                        isTrue = false;
+                    }else{
+                        writer.write(formatedString + "search: " + choice + "\n");
+                    }
+
+                } else if (selection.equals("c")){
+                    System.out.println("You have chosen to exit. Please come again! :)");
+                    writer.write(formatedString + "Exit");
+                    isTrue = false;
+                } else {
+                    System.out.println("invalid input");
+                }
+            }
+            // close the writer after while loop is false
+            writer.close();
+        } catch (IOException e) {
+            e.getStackTrace();
+            System.out.println("Error please pick a-c!");
+        }
+
+    }
+
+}
