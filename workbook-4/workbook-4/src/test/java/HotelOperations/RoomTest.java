2656fdbb0549081e5deba77905c10cfd5454cdbf
twentyfive21 <tina.winnn@gmail.com>
2024-04-30 23:12:43 -0400

    setting up junit5 tests

---
+++ workbook-4/workbook-4/src/test/java/HotelOperations/RoomTest.java
@@ -0,0 +1,27 @@
+package HotelOperations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @org.junit.jupiter.api.Test
+    void checkIn() {
+    // arrange
+    // act
+    // assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkOut() {
+    // arrange
+    // act
+    // assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void cleanRoom() {
+     // arrange
+     // act
+     // assert
+    }
+}
