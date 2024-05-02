97ed9425b923f28019bea31916229952bb6bf6f4
twentyfive21 <tina.winnn@gmail.com>
2024-05-02 14:54:49 -0400

    getting started on black jack code

--- workbook-4/workbook-4/src/main/java/blackJack/Card.java
+++ workbook-4/workbook-4/src/main/java/blackJack/Card.java
@@ -1,6 +1,6 @@
 package blackJack;
 
-public class Card {
+class Card {
     private String suit;
     private String value;
     private boolean isFaceUp;
@@ -10,39 +10,53 @@ public class Card {
         this.value = value;
         this.isFaceUp = false;
     }
-    public String getSuit(){
-// only return the suit if the card is face up
-        if(isFaceUp){
+
+    public String getSuit() {
+        // Only return the suit if the card is face up
+        if (isFaceUp) {
             return suit;
         } else {
             return "#";
         }
     }
-    public String getValue(){
-    // only return the value if the card is face up
-        if(isFaceUp){
-    // this is the string value of the card
-    // i.e. A, K, Q, J, 10, 9 ...
+
+    public String getValue() {
+        // Only return the value if the card is face up
+        if (isFaceUp) {
             return value;
         } else {
             return "#";
         }
     }
-    public int getPointValue(){
-    // only return the value if the card is face up
-        if(isFaceUp){
-    // determine point value and return it
-    // A = 11
-    // K, Q, J = 10
-    // all numeric cards are equal to their face value
+
+    public int getPointValue() {
+        if (isFaceUp) {
+            switch (value) {
+                case "A":
+                    return 11; // Ace is initially worth 11 points
+                case "K":
+                case "Q":
+                case "J":
+                    return 10; // Face cards are worth 10 points
+                default:
+                    // Numeric cards are worth their face value
+                    return Integer.parseInt(value);
+            }
         } else {
-            return 0;
+            return 0; // Face-down card has no value
         }
     }
-    public boolean isFaceUp(){
+
+    public boolean isFaceUp() {
         return isFaceUp;
     }
-    public void flip(){
+
+    public void flip() {
         isFaceUp = !isFaceUp;
+    }
+
+    @Override
+    public String toString() {
+        return value + " of " + suit;
     }
 }
--- workbook-4/workbook-4/src/main/java/blackJack/Deck.java
+++ workbook-4/workbook-4/src/main/java/blackJack/Deck.java
@@ -1,37 +1,37 @@
 package blackJack;
-
 import java.util.ArrayList;
 import java.util.Collections;
-
-public class Deck {
+class Deck {
     private ArrayList<Card> cards;
-    public Deck(){
+
+    public Deck() {
         cards = new ArrayList<>();
-        String[] suits = {"Hearts","Spades","Diamonds","Clubs"};
-        String[] values = {"2","3","4","5","6","7","8",
-                "9","10","J","Q","K","A"};
-// these loops create all the cards in the deck
-// and add them to the ArrayList
-        for(String suit: suits){
-            for(String value: values){
+        String[] suits = {"Hearts", "Spades", "Diamonds", "Clubs"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        // Create all the cards in the deck and add them to the ArrayList
+        for (String suit : suits) {
+            for (String value : values) {
                 Card card = new Card(suit, value);
                 cards.add(card);
             }
         }
     }
-    public void shuffle(){
+
+    public void shuffle() {
         Collections.shuffle(cards);
     }
-    public Card deal(){
-// deal the top card (if there are any cards left
-        if(cards.size() > 0){
-            Card card = cards.remove(0);
-            return card;
+
+    public Card deal() {
+        // Deal the top card (if there are any cards left)
+        if (cards.size() > 0) {
+            return cards.remove(0);
         } else {
             return null;
         }
     }
-    public int getSize(){
+
+    public int getSize() {
         return cards.size();
     }
 }
