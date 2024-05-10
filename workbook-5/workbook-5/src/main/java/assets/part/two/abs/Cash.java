9fba3d1954c1e2d16d54673de77e59f9aa4ab45f
twentyfive21 <tina.winnn@gmail.com>
2024-05-10 11:34:39 -0400

    cash class added with abs method

--- workbook-5/workbook-5/src/main/java/assets/part/two/abs/Cash.java
+++ workbook-5/workbook-5/src/main/java/assets/part/two/abs/Cash.java
@@ -1,2 +1,16 @@
-package assets.part.two.abs;public class Cash {
+package assets.part.two.abs;
+
+public class Cash extends Asset{
+    // private members
+    private double amount;
+
+    // constructor
+    public Cash(String description, String dateAcquired, double originalCost, double amount){
+        super(description,dateAcquired,originalCost);
+        this.amount = amount;
+    }
+
+    public double getValue(){
+        return amount;
+    }
 }
