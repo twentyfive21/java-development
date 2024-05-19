478ba1bec9d21e0cf32e856790527d7f3eb28327
twentyfive21 <tina.winnn@gmail.com>
2024-05-18 21:26:33 -0400

    added custom user greeting

--- workbook-6/git-excercise/src/main/java/com/pluralsight/Main.java
+++ workbook-6/git-excercise/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,14 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide your name! ");
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.printf("\nHello %s!", name);
     }
 }
