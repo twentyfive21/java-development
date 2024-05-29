9bb8b7489079b179d53f1ea4a85161a694a23aa1
twentyfive21 <tina.winnn@gmail.com>
2024-05-29 14:25:28 -0400

    finsihed program added extra formatting for menu

--- workbook-8/Mod03/SakilaMovies/src/main/java/com/pluralsight/Main.java
+++ workbook-8/Mod03/SakilaMovies/src/main/java/com/pluralsight/Main.java
@@ -13,7 +13,8 @@ public class Main {
 
     // declare scanner globally for use
     static Scanner scanner = new Scanner(System.in);
-
+    // create the data manager
+    static DataManager dataManager = null;
     // main method
     public static void main(String[] args) {
         // keep program running till set to exit
@@ -28,26 +29,25 @@ public class Main {
             dataSource.setUsername("root");
             dataSource.setPassword("password");
 
-            // create the data manager
-            DataManager dataManager = new DataManager(dataSource);
+            dataManager = new DataManager(dataSource);
 
             // interact with database
-            // get user input for last name
-            String lastNameOfActor = scanner.nextLine().trim();
+            String choice = scanner.nextLine();
+            switch (choice){
+                case "1" : getLastNameInfo(dataSource);
+                    break;
+                case "2" : getFullNameInfo(dataSource);
+                    break;
+                case "3" : getByIdInfo(dataSource);
+                    break;
+                case "4" : System.out.println("~~~~~~~~~ Exiting ~~~~~~~~~~~~");
+                            running = false;
+                    break;
+                default: System.out.println("Invalid input");
+                    break;
+            }
 
-            dataManager.searchByLastName(dataSource, lastNameOfActor);
 
-            System.out.println("~~ Enter the first and last name of the actor you want to see movies of ~~");
-            System.out.print("First name: ");
-            String firstName = scanner.nextLine().trim();
-            System.out.print("Last name: ");
-            String lastName = scanner.nextLine().trim();
-            dataManager.searchBy(dataSource,firstName,lastName);
-
-            System.out.println("~~ Enter the ID of the actor you want to see movies of ~~");
-            System.out.print("Actor id: ");
-            String actorId = scanner.nextLine().trim();
-            dataManager.searchBy(dataSource,actorId);
         }
 
     }
@@ -55,8 +55,36 @@ public class Main {
 
     public static void beginningPrompts(){
         System.out.println("\n~~~ Welcome to the movie information desk ~~~~");
-        System.out.println("\n~~~ Please provide the last name of an actor you like ~~~~");
+        System.out.println("(1) Search by last name");
+        System.out.println("(2) Search by full name");
+        System.out.println("(3) Search by actor id");
+        System.out.println("(4) Exit");
+        System.out.print("Selection: ");
+
+    }
+
+    public static void getLastNameInfo(DataSource dataSource){
+        // get user input for last name
         System.out.print("Selection actor last name: ");
+        String lastNameOfActor = scanner.nextLine().trim();
+        dataManager.searchByLastName(dataSource, lastNameOfActor);
+
+    }
+
+    public static void getFullNameInfo(DataSource dataSource){
+        System.out.println("~~ Enter the first and last name of the actor you want to see movies of ~~");
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine().trim();
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine().trim();
+        dataManager.searchBy(dataSource,firstName,lastName);
+    }
+
+    public static void getByIdInfo(DataSource dataSource){
+        System.out.println("~~ Enter the ID of the actor you want to see movies of ~~");
+        System.out.print("Actor id: ");
+        String actorId = scanner.nextLine().trim();
+        dataManager.searchBy(dataSource,actorId);
     }
 
 
